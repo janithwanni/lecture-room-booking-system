10a569fcf9dc893876f2481bde6da331a483c4ec
janithwanni <r.t.doram@gmail.com>
2018-07-09 21:08:04 +0530

    "vs-commit-before-changes-update"

--- src/app/bookings/statusview/statusatglance/statusatglance.component.ts
+++ src/app/bookings/statusview/statusatglance/statusatglance.component.ts
@@ -1,15 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { HallInfoManagerService } from "../../../shared/services/hall-info-manager.service";
+import { Observable, of } from "rxjs";
 
 @Component({
-  selector: 'app-statusatglance',
-  templateUrl: './statusatglance.component.html',
-  styleUrls: ['./statusatglance.component.scss']
+  selector: "app-statusatglance",
+  templateUrl: "./statusatglance.component.html",
+  styleUrls: ["./statusatglance.component.scss"]
 })
 export class StatusatglanceComponent implements OnInit {
+  status: {
+    now: Observable<number>;
+    tommorrow: Observable<number>;
+    nextweek: Observable<number>;
+  } = { now: of(0), tommorrow: of(0), nextweek: of(0) };
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private hallinfo: HallInfoManagerService) {
+    this.setupComponent();
   }
 
+  setupComponent() {
+    const currDate = new Date();
+    this.status.now = this.hallinfo.getFreeHalls(
+      currDate.getFullYear() + "",
+      currDate.getMonth() + "",
+      currDate.getDate() + ""
+    );
+    this.status.tommorrow = this.hallinfo.getFreeHalls(
+      currDate.getFullYear() + "",
+      currDate.getMonth() + "",
+      currDate.getDate() + 1 + ""
+    );
+    const nextweek = new Date(currDate.getTime() + 7 * 24 * 60 * 60 * 1000);
+    this.status.nextweek = this.hallinfo.getFreeHalls(
+      nextweek.getFullYear() + "",
+      nextweek.getMonth() + "",
+      nextweek.getDate() + 1 + ""
+    );
+
+    this.status.now.subscribe(data => {
+      console.log(data);
+    });
+  }
+  ngOnInit() {}
 }
--- src/app/shared/services/hall-info-manager.service.ts
+++ src/app/shared/services/hall-info-manager.service.ts
@@ -1,26 +1,83 @@
-import { Injectable } from '@angular/core';
-import { AngularFireDatabase } from 'angularfire2/database';
-import { Observable } from 'rxjs';
-import { take, takeLast } from 'rxjs/operators';
+import { Injectable } from "@angular/core";
+import { AngularFireDatabase } from "angularfire2/database";
+import { Observable, of, concat } from "rxjs";
+import {
+  take,
+  takeLast,
+  count,
+  isEmpty,
+  flatMap,
+  scan,
+  map
+} from "rxjs/operators";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class HallInfoManagerService {
+  constructor(private db: AngularFireDatabase) {}
 
-  constructor(private db: AngularFireDatabase) { }
-
-  getTimeSlot(): string{
-    return null;
+  isCampusTime(): boolean {
+    const currentDate = new Date();
+    if (currentDate.getHours() >= 8 && currentDate.getHours() <= 18) {
+      return true;
+    } else {
+      return false;
+    }
   }
-  getFreeHalls(year:string,month:string,day:string): Observable<any>{
+  getTimeSlot(): string {
+    if (this.isCampusTime()) {
+      var currentDate = new Date();
+      var firstChar = currentDate.getHours() - 8;
+      var secondChar = currentDate.getMinutes() <= 30 ? 0 : 1;
+      return "time-" + firstChar + secondChar + "";
+    } else {
+      return null;
+    }
+  }
+
+  getFreeHalls(year: string, month: string, day: string): Observable<any> {
     //retrieves the number of halls free right now at the given day
-    return null;
+    if (this.isCampusTime()) {
+      return this.db
+        .list("/root/time-bookings/" + year + "/" + month + "/" + day)
+        .valueChanges()
+        .pipe(
+          isEmpty(),
+          flatMap(b => {
+            if (!b) {
+              return this.db
+                .list("/root/time-bookings/" + year + "/" + month + "/" + day)
+                .valueChanges()
+                .pipe(count());
+            } else {
+              return this.getTotalNumberofHalls();
+            }
+          }),
+          flatMap(c => {
+            console.log(c);
+            return concat(of(c), this.getTotalNumberofHalls());
+          }),
+          scan((x, y) => {
+            return Math.abs(x - y);
+          })
+        );
+    } else {
+      return null;
+    }
   }
 
-  getHalls():Observable<any>{
-    var items:Observable<any> = this.db.list('/root/lecture-halls').valueChanges();
+  getTotalNumberofHalls(): Observable<number> {
+    return this.db
+      .list("/root/lecture-halls/")
+      .valueChanges()
+      .pipe(count());
+  }
+
+  getHalls(): Observable<any> {
+    var items: Observable<any> = this.db
+      .list("/root/lecture-halls")
+      .valueChanges();
     return items;
   }
-  
 }
